b02039aaa69f88e398a2cd27420f3510d30ea9c9
Sivatharan <sivatharanoff97@gmail.com>
2026-01-15 19:53:21 +0530

    add google font

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 final theme = ThemeData(
   colorScheme: ColorScheme.fromSeed(
     brightness: Brightness.dark,
     seedColor: const Color.fromARGB(255, 131, 57, 0),
   ),
+  textTheme: GoogleFonts.latoTextTheme(),
 );
 void main() {
   runApp(const App());
@@ -16,6 +18,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: theme,
       // home: //todo,
     );
