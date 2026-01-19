edaa3b792f0168bbd4f79db5b861da19b98ecb34
Sivatharan <sivatharanoff97@gmail.com>
2026-01-19 20:23:59 +0530

    add drawer

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:meals/screens/tabs.dart';
 final theme = ThemeData(
   colorScheme: ColorScheme.fromSeed(
     brightness: Brightness.dark,
-    seedColor: const Color.fromARGB(255, 255, 77, 0),
+    seedColor: const Color.fromARGB(255, 4, 255, 142),
   ),
   textTheme: GoogleFonts.latoTextTheme(),
 );
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/meals.dart';
+import 'package:meals/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -47,6 +48,14 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
+  void _setScreen(String indentifier) {
+    if (indentifier == 'filters') {
+      //
+    } else {
+      Navigator.of(context).pop();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget activePage = CategoriesScreen(
@@ -65,6 +74,9 @@ class _TabsScreenState extends State<TabsScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(activePageTitle),
+      ),
+      drawer: MainDrawer(
+        onSelectScreen: _setScreen,
       ),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
