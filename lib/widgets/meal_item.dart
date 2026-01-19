4f57caa100440470595e20720a545ebf43f66cdf
Sivatharan <sivatharanoff97@gmail.com>
2026-01-19 06:22:32 +0530

    finish meal detail screen

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -4,9 +4,10 @@ import 'package:transparent_image/transparent_image.dart';
 import 'package:meals/widgets/meal_item_trait.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.meal});
+  const MealItem({super.key, required this.meal, required this.onSelectMeal});
 
   final Meal meal;
+  final void Function(Meal meal) onSelectMeal;
 
   String get complexity {
     return meal.complexity.name[0].toUpperCase() +
@@ -28,7 +29,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
